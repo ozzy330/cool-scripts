3652f846464818ce8e0a94f9c9b3070e51c4ca02
ozzy330 <oskar.quesada30@gmail.com>
2024-12-16 21:25:00 -0600

    Remove Docker

--- rocky-defaults.sh
+++ rocky-defaults.sh
@@ -1,8 +1,5 @@
 sudo dnf install epel-release -y && sudo dnf update -y &&\
-sudo dnf install 'dnf-command(config-manager)' -y &&\
-sudo dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &&\
 sudo dnf install git neovim wget go screen bash-completion fontconfig unzip \
-docker-ce docker-ce-cli containerd.io docker-compose-plugin -y &&\
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.zip &&\
 sudo unzip FiraCode.zip -d /usr/share/fonts/ &&\
 rm FiraCode.zip &&\
