081bc7d38d1c2ca4fb332c9625a818cb30c94950
ozzy330 <oskar.quesada30@gmail.com>
2024-12-16 21:48:31 -0600

    Fix unzip to use overwrite

--- rocky-min.sh
+++ rocky-min.sh
@@ -1,8 +1,8 @@
-# rocky-min v1.1
+# rocky-min v1.2
 sudo dnf install epel-release -y && sudo dnf update -y &&\
 sudo dnf install git neovim wget go screen bash-completion fontconfig unzip tar -y &&\
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/FiraCode.zip &&\
-sudo unzip FiraCode.zip -d /usr/share/fonts/ &&\
+sudo unzip -o FiraCode.zip -d /usr/share/fonts/ &&\
 rm -f FiraCode.zip &&\
 curl -sS https://starship.rs/install.sh | sh &&\
 echo "eval "$(starship init bash)"" >> ~/.bashrc
