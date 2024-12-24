320ea006e8ace98fbb710bf5b09c0470fc102cb1
ozzy330 <oskar.quesada30@gmail.com>
2024-12-24 02:28:58 -0600

    Remove not working tool

--- rocky-igpu.sh
+++ rocky-igpu.sh
@@ -6,5 +6,3 @@ sudo dnf install https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-9.
 sudo dnf install https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-9.noarch.rpm -y
 # Drivers for Broadwell (5th gen) or newer and some utils
 sudo dnf install -y intel-media-driver libva-utils 
-# For intel_gpu_top
-sudo dnf install -y https://dl.rockylinux.org/pub/rocky/8/AppStream/x86_64/os/Packages/i/intel-gpu-tools-2.99.917-41.20210115.el8.x86_64.rpm
